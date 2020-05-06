0cfb1e1757c9c09009575b1cf67483214f14340f
Maphick <maphick@yandex.ru>
2020-05-06 23:21:49 +0300

    feat: subtask 2 done

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,25 @@ package subtask2
 
 class Combinator {
 
+    fun Fact(n:Int):Long {
+        if (n<=1)
+            return 1
+        else
+            return n*Fact(n-1)
+    }
+
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        if (array.size < 2)
+            return null
+        var m = array[0]
+        var n:Int = array[1]
+        var number = Fact(n)/m
+        for (x in 1..n-1)
+        {
+            if (Fact(x)*Fact(n-x) == number)
+                return x
+        }
+        return null
     }
 }
