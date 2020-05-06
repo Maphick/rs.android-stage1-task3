e10ffd823bdbef25b2332bfcac3aa5758cc01b66
Maphick <maphick@yandex.ru>
2020-05-06 23:21:10 +0300

    feat: subtask 1 done

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -4,6 +4,43 @@ class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        var pow = numbers.size - 1
+        if (pow<0)
+            return null
+        var polinom = ""
+        while (pow >= 0) {
+            val koeff = numbers[numbers.size - 1 - pow]
+            if (koeff == 0)
+            {
+                pow -= 1
+                continue
+            }
+            if (koeff == 1) {
+
+            } else if (koeff == -1) {
+                polinom += " - "
+            } else if (koeff > 0) {
+                polinom += " + " + koeff
+            }
+            else if (koeff < 0) {
+                polinom += " - " + Math.abs(koeff)
+            }
+
+            if (pow == 1)
+            {
+                polinom += "x"
+            }
+            else if (pow == 0)
+            {
+
+            }
+            else
+            {
+                polinom += "x^" + pow
+            }
+            pow -= 1
+        }
+        polinom = polinom.substring(3,polinom.length)
+        return polinom
     }
 }
